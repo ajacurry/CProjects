f5c71dadc7dbc138cac2f4a6a70d90b076567985
Aja Brittany Curry <104596187+ajacurry@users.noreply.github.com>
2022-05-06 21:42:38 -0400

    Add files via upload

---
+++ NestedForLoops.cpp
@@ -0,0 +1,20 @@
+//Aja Curry
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "COUNTING " << endl;
+
+    for (num = 1; num <= 10; num++)
+    {
+        cout << num << endl;
+    }
+
+    cout << "Final value if num is: " << num << endl;
+
+    return 0;
+}
